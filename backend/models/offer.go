6dfb583c103db8a7b46b9520483140898eeb05ec
Kerlyn Difo <100390581+KerlynD@users.noreply.github.com>
2025-10-16 19:08:31 -0400

    Added offers page

--- backend/models/offer.go
+++ backend/models/offer.go
@@ -3,12 +3,14 @@ package models
 import "time"
 
 type Offer struct {
-	ID          int       `json:"id"`
-	UserID      int       `json:"user_id"`
-	Company     string    `json:"company"`
-	Role        string    `json:"role"`
-	HourlyRate  float64   `json:"hourly_rate"`
-	MonthlyRate float64   `json:"monthly_rate"`
-	Location    string    `json:"location"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID             int       `json:"id"`
+	UserID         int       `json:"user_id"`
+	Company        string    `json:"company"`
+	CompanyLogoURL string    `json:"company_logo_url"`
+	Role           string    `json:"role"`
+	OfferType      string    `json:"offer_type"` // "internship" or "full-time"
+	HourlyRate     float64   `json:"hourly_rate"`
+	MonthlyRate    float64   `json:"monthly_rate"`
+	Location       string    `json:"location"`
+	CreatedAt      time.Time `json:"created_at"`
 }
